b0c2b66a90cc3e0bedefb4bbe787def1adc6c7b9
Juan Carlos Bautista <jcba_20@hotmail.com>
2021-07-29 22:15:01 -0500

    Login y Registro terminado

---
+++ lib/routes/routes.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:chat/pages/chat_page.dart';
+import 'package:chat/pages/loading_page.dart';
+import 'package:chat/pages/login_page.dart';
+import 'package:chat/pages/register_page.dart';
+import 'package:chat/pages/usuarios_page.dart';
+import 'package:flutter/material.dart';
+
+final Map<String, Widget Function(BuildContext)> appRoutes = {
+
+  'chat'    : (_) => ChatPage(),
+  'loading' : (_) => LoadingPage(),
+  'login'   : (_) => LoginPage(),
+  'register': (_) => RegisterPage(),
+  'usuarios': (_) => UsuariosPage(),
+
+};
