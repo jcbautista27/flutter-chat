94bf036a2a3eac7610badfc13bd08515eb6582c7
Juan Carlos Bautista <jcba_20@hotmail.com>
2021-08-27 21:58:43 -0500

    Fin seccion 9 - Login y Registro

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,9 +1,12 @@
+import 'package:chat/helpers/mostrar_alerta.dart';
+import 'package:chat/services/auth_service.dart';
 import 'package:chat/widgets/boton_azul.dart';
 import 'package:flutter/material.dart';
 
 import 'package:chat/widgets/custom_input.dart';
 import 'package:chat/widgets/labels.dart';
 import 'package:chat/widgets/logo.dart';
+import 'package:provider/provider.dart';
 
 
 class RegisterPage extends StatelessWidget {
@@ -58,6 +61,7 @@ class __FormState extends State<_Form> {
 
   @override
   Widget build(BuildContext context) {
+    final authSertvice = Provider.of<AuthService>(context);
     return Container(
       margin: EdgeInsets.only(top: 40),
       padding: EdgeInsets.symmetric( horizontal: 50),
@@ -84,10 +88,19 @@ class __FormState extends State<_Form> {
           ),
 
           BotonAzul(
-            text: 'Ingresar', 
-            onPressed: (){
-              print(emailCtrl.text);
-              print(passCtrl.text);
+            text: 'Crear cuenta', 
+            onPressed: authSertvice.autenticando? null : () async{
+
+              FocusScope.of(context).unfocus();
+
+              final registerOk = await authSertvice.register(nameCtrl.text.trim(), emailCtrl.text.trim(), passCtrl.text.trim());
+
+              if (registerOk == true){
+                Navigator.pushReplacementNamed(context, 'usuarios');
+              }else{
+                mostrarAlerta(context, 'Registro incorrecto', registerOk);
+              }
+              
             }
           ),
           
